028626ec8300fb2f634d3053a6bbbdff922cc4bd
Konstantin <gladyi@hotmail.com>
2021-07-17 13:56:36 +0300

    init commit

---
+++ src/main/java/com/gik/market/market/MarketApplication.java
@@ -0,0 +1,17 @@
+package com.gik.market.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
